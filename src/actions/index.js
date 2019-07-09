48b0815a26e422f39b2c997ed757ca64187e1d06
Trent Davis <trendavisinc@gmail.com>
2019-07-09 18:22:14 -0500

    API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import streams from '../apis/streams';
+import { async } from 'q';
 
 export const signIn = userID => {
     return {
@@ -11,4 +13,10 @@ export const signOut = () => {
     return {
         type: SIGN_OUT
     };
+};
+
+export const createStream = formValues => async dispatch => {
+    const response = await streams.post('/streams', formValues);
+
+    dispatch({ type: CREATE_STREAM, payload: response.data });
 };
--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
-    renderInput({ input, label, meta }) {
-        console.log(meta)
+    renderError({ error, touched }) {
+        if(touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+    renderInput = ({ input, label, meta }) => {       
+        const style = `field ${meta.error && meta.touched ? 'error' : ''}`; 
         return ( 
-            <div className="field">
+            <div className={style}>
                 <label>{label}</label>
-                <input {...input} />
-                <div>{meta.error}</div>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
             </div>        
         );
     }
-    onSubmit(formValues) {
-       console.log(formValues)
+    onSubmit = formValues => {
+       this.props.createStream(formValues);
     }
     render() {
         return (
-            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+            <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button type="submit" className="ui button primary">Submit</button>
@@ -39,7 +50,9 @@ const validate = formValues => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
