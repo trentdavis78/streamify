ee6a59b485ef532f5584f3d26d2b2a5734ac8613
Trent Davis <trendavisinc@gmail.com>
2019-07-10 15:52:40 -0500

    Section 265

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -7,7 +9,6 @@ import {
     DELETE_STREAM,
     EDIT_STREAM
  } from './types';
-import streams from '../apis/streams';
 
 export const signIn = userID => {
     return {
@@ -20,9 +21,11 @@ export const signOut = () => {
         type: SIGN_OUT
     };
 };
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    history.push('/');
 };
 export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
